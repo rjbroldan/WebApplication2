f6d073dfc664408db941b51e079395fb55ae26b9
rjbroldan <30530178+rjbroldan@users.noreply.github.com>
2017-07-30 23:37:29 +1000

    300720171137
    Data binding for Customers

--- WebApplication2/ViewModels/NewCustomerViewModel.cs
+++ WebApplication2/ViewModels/NewCustomerViewModel.cs
@@ -6,7 +6,7 @@ using WebApplication2.Models;
 
 namespace WebApplication2.ViewModels
 {
-    public class NewCustomerViewModel
+    public class CustomerFormViewModel
     {
         public IEnumerable<MembershipType> MembershipTypes { get; set; }
         public Customer Customer { get; set; }
